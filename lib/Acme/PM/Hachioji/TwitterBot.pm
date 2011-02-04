feeabf9dcc4f48528f7c4fed067a199b0af2a661
YellowTurtle / satoshi azuma <ytnobody@gmail.com>
2011-02-05 02:28:08 +0900

    initial commit

---
+++ lib/Acme/PM/Hachioji/TwitterBot.pm
@@ -0,0 +1,32 @@
+package Acme::PM::Hachioji::TwitterBot;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::PM::Hachioji::TwitterBot -
+
+=head1 SYNOPSIS
+
+  use Acme::PM::Hachioji::TwitterBot;
+
+=head1 DESCRIPTION
+
+Acme::PM::Hachioji::TwitterBot is
+
+=head1 AUTHOR
+
+satoshi azuma E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
